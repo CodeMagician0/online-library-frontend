70557a88d287017d210c87ec8e59da4ed6f98f2f
SiuyunYip <yzyyezhaoyuan@gmail.com>
2023-09-13 17:10:05 +0100

    delete: rename file

--- src/services/apis.js
+++
@@ -1,7 +0,0 @@
-const API_BASE_URL = "http://localhost:8080/api";
-
-export const BOOK_ROUTES = {
-  getBooks: `${API_BASE_URL}/books`,
-  getCategories: `${API_BASE_URL}/books/categories`,
-  searchBook: `${API_BASE_URL}/books/search`,
-};
