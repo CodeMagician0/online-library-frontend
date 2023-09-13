fec8961876fdabcfe13ff98f2a7f08115a316048
SiuyunYip <yzyyezhaoyuan@gmail.com>
2023-09-13 17:07:02 +0100

    add: custom fetch with authorization header

---
+++ src/layouts/Utils/useFetch.ts
@@ -0,0 +1,27 @@
+import { useContext } from "react";
+import AuthContext from "../../context/AuthContext";
+
+const useFetch = () => {
+  const { authTokens } = useContext(AuthContext);
+
+  let originalRequest = async (url: string, config: any) => {
+    let rsp = await fetch(url, config);
+    let data = await rsp.json();
+    return { rsp, data };
+  };
+
+  let callFetch = async (url: string, config: any) => {
+    if (authTokens) {
+      config["headers"] = {
+        Authorization: `Bearer ${authTokens}`,
+      };
+    }
+
+    let { rsp, data } = await originalRequest(url, config);
+    return { rsp, data };
+  };
+
+  return callFetch;
+};
+
+export default useFetch;
